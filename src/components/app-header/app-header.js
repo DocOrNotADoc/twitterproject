af59e97eb06fd921d2d51929aefdbf12362306f0
DocOrNotADoc <leesunsinn@protonmail.com>
2020-08-15 18:40:12 +0300

    structure&styles created

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css';
+
+const AppHeader = () => {
+    return(
+        <div className="app-header d-flex">
+            <h1>Jhonny Depp</h1>
+            <h2>6 записей, из них понравилось 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
