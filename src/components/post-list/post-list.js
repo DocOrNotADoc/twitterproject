c673e5c9cbd50d10a0d23a7a98248483eb674dae
DocOrNotADoc <leesunsinn@protonmail.com>
2020-09-15 09:00:31 +0300

    failed to meke class ListItem

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,39 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PostListItem from '../post-list-item/post-list-item';
 import {ListGroup} from 'reactstrap';
 import './post-list.css';
 
-const PostList = ({posts, onDelete, onToggleImportant, onToggleLike}) => {
+export default class PostList extends Component {
 
-    const elements = posts.map((item) => {
+    posts = this.state;
 
-        if ( typeof item === 'object' && isEmpty(item) ){ 
-            const {id, ...itemProps} = item;
-            return (
-                <li key = {id} className='list-group-item'>
-                    <PostListItem
-                        {...itemProps}
-                        onDelete={() => onDelete(id)}
-                        onToggleImportant={() => onToggleImportant(id)}
-                        onToggleLike={() => onToggleLike(id)}/>
-                </li>
-            )
-        }
-    });
+    // {onDelete, onToggleImportant, onToggleLike} = this.props;
 
-    function isEmpty(obj) {
+    isEmpty = obj => {
         for(let key in obj)
         {
             return true;
         }
             return false;
     }
+    
+    render() {
 
-    return (
-        <ListGroup className='app-list'>
-            {elements}
-        </ListGroup>
-    )
+        const elements = (posts) => {
+            posts.map((item) => {
+    
+                if ( typeof item === 'object' && this.isEmpty(item) ){ 
+                    const {id, ...itemProps} = item;
+                    return (
+                        <li key = {id} className='list-group-item'>
+                            <PostListItem
+                                {...itemProps}
+                                onDelete={() => this.props.onDelete(id)}
+                                onToggleImportant={() => this.props.onToggleImportant(id)}
+                                onToggleLike={() => this.props.onToggleLike(id)}/>
+                        </li>
+                    )
+                }
+            });
+        }
+
+        return (
+            <ListGroup className='app-list'>
+                {elements}
+            </ListGroup>
+        )
+    }
 }
 
-export default PostList;
+
+
+// const PostList = ({posts, onDelete, onToggleImportant, onToggleLike}) => {
+
+//     const elements = posts.map((item) => {
+
+//         if ( typeof item === 'object' && isEmpty(item) ){ 
+//             const {id, ...itemProps} = item;
+//             return (
+//                 <li key = {id} className='list-group-item'>
+//                     <PostListItem
+//                         {...itemProps}
+//                         onDelete={() => onDelete(id)}
+//                         onToggleImportant={() => onToggleImportant(id)}
+//                         onToggleLike={() => onToggleLike(id)}/>
+//                 </li>
+//             )
+//         }
+//     });
+
+//     function isEmpty(obj) {
+//         for(let key in obj)
+//         {
+//             return true;
+//         }
+//             return false;
+//     }
+
+//     return (
+//         <ListGroup className='app-list'>
+//             {elements}
+//         </ListGroup>
+//     )
+// }
+
+// export default PostList;
